88eb1ec129a92eeb9bb9f7d133179ccac550aafc
Jayke770 <jakevelasco40@gmail.com>
2022-04-18 15:39:12 +0800

    .

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,13 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import FacebookProvider from 'next-auth/providers/facebook'
 import GitHubProvider from "next-auth/providers/github"
-import Credentials from 'next-auth/providers/credentials'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import clientPromise from "../../../lib/mongodb"
+import Users from '../../../lib/Users'
+import moment from "moment"
+import { nanoid } from 'nanoid'
+import { uniqueNamesGenerator as generateUsername, adjectives, colors, animals } from 'unique-names-generator'
+import { ObjectId } from "mongodb"
 export default NextAuth({
     secret: process.env.SECRET,
     providers: [
@@ -19,11 +23,6 @@ export default NextAuth({
         GitHubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
-        }),
-        Credentials({
-            async authorize(credentials, req) {
-
-            }
         })
     ],
     adapter: MongoDBAdapter(clientPromise),
@@ -41,8 +40,39 @@ export default NextAuth({
             return session
         }
     },
+    events: {
+        async signIn({ user, account, profile, isNewUser }) {
+            if (isNewUser) {
+                const username = `${generateUsername({ dictionaries: [adjectives, colors, animals], length: 2 })}-${nanoid(3)}`
+                await Users.updateOne({ _id: { $eq: ObjectId(user.id) } }, {
+                    $set: {
+                        emailVerified: true,
+                        username: username,
+                        status: 1,
+                        lastSeen: moment().format(),
+                        created: moment().format()
+                    }
+                })
+            } else {
+                await Users.updateOne({ _id: { $eq: ObjectId(user.id) } }, {
+                    $set: {
+                        lastSeen: moment().format(),
+                        status: 1
+                    }
+                })
+            }
+        },
+        async signOut({ session, token }) {
+            await Users.updateOne({ _id: { $eq: ObjectId(token.sub) } }, {
+                $set: {
+                    lastSeen: moment().format(),
+                    status: 0
+                }
+            })
+        }
+    },
     pages: {
         signIn: '/auth/login',
-        signOut: '/home'
+        signOut: '/'
     }
 })
